e634d50be2b02e0fe24b3ac8a37d149773f15252
mbalcer <mateusz75319@gmail.com>
2019-11-27 21:58:13 +0100

    Initialize commit

---
+++ src/main/java/zpo/project/fuelscanner/FuelScannerApplication.java
@@ -0,0 +1,13 @@
+package zpo.project.fuelscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelScannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuelScannerApplication.class, args);
+    }
+
+}
