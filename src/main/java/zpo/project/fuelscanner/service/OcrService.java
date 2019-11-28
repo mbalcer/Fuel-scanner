3cbe86f288eedbd1a98918634a3f9e7a67371275
mbalcer <mateusz75319@gmail.com>
2019-11-28 11:10:29 +0100

    Added configuration to database and languages path

--- src/main/java/zpo/project/fuelscanner/service/OcrService.java
+++ src/main/java/zpo/project/fuelscanner/service/OcrService.java
@@ -2,6 +2,7 @@ package zpo.project.fuelscanner.service;
 
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.imageio.ImageIO;
@@ -11,12 +12,15 @@ import java.net.URL;
 @Service
 public class OcrService {
 
+    @Value("${languages.path}")
+    private String languagesPath;
+
     public String doOcr(String url) {
         try {
             URL imageFile = new URL(url);
             BufferedImage bufferedImage = ImageIO.read(imageFile);
             ITesseract instance = new Tesseract();
-            instance.setDatapath("src/main/resources/languages");
+            instance.setDatapath(languagesPath);
             instance.setLanguage("pol");
             return instance.doOCR(bufferedImage);
         } catch (Exception e) {
