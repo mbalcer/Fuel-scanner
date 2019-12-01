2c70b89474a0bc3beb027c5ff29f20b0956c96f7
matkuc003 <mnorris.k97@gmail.com>
2019-12-01 21:20:19 +0100

    Fixxx

--- src/main/java/zpo/project/fuelscanner/service/FuelSumService.java
+++ src/main/java/zpo/project/fuelscanner/service/FuelSumService.java
@@ -44,14 +44,14 @@ public class FuelSumService {
     }
     private static MultiValuedMap<Double,Double> getCount(final String str) {
         final Pattern p = Pattern.compile("\\s*PLN.?([\\d+]{1,5}\\.?[\\d]{0,5})",Pattern.DOTALL);
-        final Pattern p2 = Pattern.compile("\\bVERVA 95\\S*\\(?\\d?\\)?\\s*([\\d+]{1,5}\\.?[\\d]{0,5})\\b|\\bVERVA 98\\S*\\(?\\d?\\)?\\s*\\b\\s*([\\d+]{1,5}\\.?[\\d]{0,5})|\\bGAZ LPG\\S*\\(?\\d?\\)?\\s*\\b\\s*([\\d+]{1,5}\\.?[\\d]{0,5})"); // w tym przypadku dodamy wiecej nazw
+        final Pattern p2 = Pattern.compile("\\b[.?]LEJ [.?]APĘDOWY\\S*\\(?\\d?\\)?\\s*([\\d+]{1,5}\\.?[\\d]{0,5})\\b|\\bVERVA 95\\S*\\(?\\d?\\)?\\s*([\\d+]{1,5}\\.?[\\d]{0,5})\\b|\\bVERVA 98\\S*\\(?\\d?\\)?\\s*\\b\\s*([\\d+]{1,5}\\.?[\\d]{0,5})|\\bGAZ LPG\\S*\\(?\\d?\\)?\\s*\\b\\s*([\\d+]{1,5}\\.?[\\d]{0,5})"); // w tym przypadku dodamy wiecej nazw
 
         final MultiValuedMap<Double,Double> countValues = new ArrayListValuedHashMap<>();
         final Matcher matcher = p.matcher(str);
         final Matcher matcher2 = p2.matcher(str);
 
         while (matcher.find() && matcher2.find()) {
-            countValues.put(Double.parseDouble(matcher2.group().replaceAll("\\bVERVA 98\\S*\\(?\\d?\\)?\\s*\\b|\\bGAZ LPG\\S*\\(?\\d?\\)?\\s*\\b\\b|\\bVERVA 95\\S*\\(?\\d?\\)?\\s*\\b","")),Double.parseDouble(matcher.group(1)));// w tym przypadku dodamy wiecej nazw
+            countValues.put(Double.parseDouble(matcher2.group().replaceAll("\\b[.?]LEJ [.?]APĘDOWY\\S*\\(?\\d?\\)?\\s*\\b|\\bVERVA 98\\S*\\(?\\d?\\)?\\s*\\b|\\bGAZ LPG\\S*\\(?\\d?\\)?\\s*\\b\\b|\\bVERVA 95\\S*\\(?\\d?\\)?\\s*\\b","")),Double.parseDouble(matcher.group(1)));// w tym przypadku dodamy wiecej nazw
 
         }
         return countValues;
