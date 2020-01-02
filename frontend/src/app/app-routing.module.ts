2a71c12bab79ade335bd2982ab10359820ce7d72
mbalcer <mateusz75319@gmail.com>
2020-01-02 17:27:31 +0100

    Created login-panel and dashboard component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {LoginPanelComponent} from "./login-panel/login-panel.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: '',
+    component: LoginPanelComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,14 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {LoginPanelComponent} from './login-panel/login-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginPanelComponent
   ],
   imports: [
     BrowserModule,
