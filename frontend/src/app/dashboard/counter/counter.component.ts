07058ed786296e1029a30dd4a8391503c1d12844
mbalcer <mateusz75319@gmail.com>
2020-01-03 21:27:19 +0100

    Added history of counter state to counter component

--- frontend/src/app/dashboard/counter/counter.component.ts
+++ frontend/src/app/dashboard/counter/counter.component.ts
@@ -6,9 +6,24 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent implements OnInit {
+  displayedColumns: string[] = ['counter', 'date'];
+  dataSource = EXAMPLE_DATA;
 
   constructor() { }
 
   ngOnInit() {
   }
+
 }
+
+export interface StateCounter {
+  counter: number;
+  date: string;
+}
+
+const EXAMPLE_DATA: StateCounter[] = [
+  {counter: 100000, date: "01/12/2019"},
+  {counter: 101032, date: "11/12/2019"},
+  {counter: 102333, date: "27/12/2019"},
+  {counter: 102875, date: "03/01/2020"}
+];
