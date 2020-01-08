b3bb8b4f1e93f19c94716f1da3acbd5d650f0254
mbalcer <mateusz75319@gmail.com>
2020-01-08 20:04:30 +0100

    Added fuel column to counter view

--- frontend/src/app/dashboard/counter/counter.component.ts
+++ frontend/src/app/dashboard/counter/counter.component.ts
@@ -6,7 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent implements OnInit {
-  displayedColumns: string[] = ['counter', 'date'];
+  displayedColumns: string[] = ['counter', 'date', 'fuel'];
   dataSource = EXAMPLE_DATA;
 
   constructor() { }
@@ -17,13 +17,14 @@ export class CounterComponent implements OnInit {
 }
 
 export interface StateCounter {
-  counter: number;
-  date: string;
+  counter: number,
+  fuel: number,
+  date: string
 }
 
 const EXAMPLE_DATA: StateCounter[] = [
-  {counter: 100000, date: "01/12/2019"},
-  {counter: 101032, date: "11/12/2019"},
-  {counter: 102333, date: "27/12/2019"},
-  {counter: 102875, date: "03/01/2020"}
+  {counter: 100000, fuel: 5, date: "01/12/2019"},
+  {counter: 101032, fuel: 15.5, date: "11/12/2019"},
+  {counter: 102333, fuel: 30, date: "27/12/2019"},
+  {counter: 102875, fuel: 10.23, date: "03/01/2020"}
 ];
