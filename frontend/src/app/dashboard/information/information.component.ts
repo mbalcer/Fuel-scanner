a4fec86381a8d267ed4f43b86f13424203fdf2a0
mbalcer <mateusz75319@gmail.com>
2020-01-08 22:18:06 +0100

    Created information component

--- frontend/src/app/dashboard/information/information.component.ts
+++ frontend/src/app/dashboard/information/information.component.ts
@@ -1,15 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-information',
   templateUrl: './information.component.html',
   styleUrls: ['./information.component.css']
 })
-export class InformationComponent implements OnInit {
-
+export class InformationComponent {
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
+
+
