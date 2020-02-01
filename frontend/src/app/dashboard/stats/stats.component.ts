0169cfd719a74c65093dd43faf5272f8ca1fbf13
mbalcer <mateusz75319@gmail.com>
2020-02-01 19:19:00 +0100

    Added stats with fuel usage

--- frontend/src/app/dashboard/stats/stats.component.ts
+++ frontend/src/app/dashboard/stats/stats.component.ts
@@ -17,8 +17,8 @@ export class StatsComponent implements OnInit {
       {value: "tank", viewValue: "Suma tankowania"},
       {value: "fuel", viewValue: "Koszty paliwa"},
       {value: "km", viewValue: "Przejechany dystans"},
-      {value: "usage", viewValue: "Zużycie paliwa"}
-
+      {value: "usage", viewValue: "Zużyte paliwo"},
+      {value: "usage100km", viewValue: "Zużycie paliwa na 100 km"}
   ];
   selectedStatsType: StatsType = {
       value: '',
@@ -58,17 +58,25 @@ export class StatsComponent implements OnInit {
     counterData.forEach(i => {
         if (this.selectedStatsType.value == 'km')
             this.dataChart.push([i.startLocalDate + " - " + i.endLocalDate, i.distanceTravelled]);
+        else if (this.selectedStatsType.value == 'usage')
+            this.dataChart.push([i.startLocalDate + " - " + i.endLocalDate, i.fuelConsumed]);
+        else if (this.selectedStatsType.value == 'usage100km')
+            this.dataChart.push([i.startLocalDate + " - " + i.endLocalDate, i.avgFuelConsumedOn100km]);
     });
   }
 
   changeStats(statsType) {
     this.selectedStatsType = statsType;
     if (this.selectedStatsType.value == 'tank')
-        this.columnNames = ['Data', 'Koszt paliwa', "Zatankowane paliwo"];
+        this.columnNames = ['Data', 'Koszt paliwa', 'Zatankowane paliwo'];
     else if (this.selectedStatsType.value == 'fuel')
-        this.columnNames = ['Data', 'Minimalna cena paliwa', "Srednia cena paliwa", "Maksymalna cena paliwa"];
+        this.columnNames = ['Data', 'Minimalna cena paliwa', 'Średnia cena paliwa', 'Maksymalna cena paliwa'];
     else if (this.selectedStatsType.value == 'km')
-        this.columnNames = ['Data', "Przejechane kilometry"];
+        this.columnNames = ['Data', 'Przejechane kilometry'];
+    else if (this.selectedStatsType.value == 'usage')
+        this.columnNames = ['Data', 'Zużyte paliwo'];
+    else if (this.selectedStatsType.value == 'usage100km')
+        this.columnNames = ['Data', 'Średnie zużycie paliwa na 100 km'];
 
     this.prepareData(this.receiptStats, this.counterStats);
   }
