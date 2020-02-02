9a557d69305a5d3152d760ee7b9d8d22b8fefe79
mbalcer <mateusz75319@gmail.com>
2020-02-02 21:29:52 +0100

    Fixed saving of receipt for logging user

--- frontend/src/app/dashboard/scanner/scanner.component.ts
+++ frontend/src/app/dashboard/scanner/scanner.component.ts
@@ -3,6 +3,8 @@ import {Component, OnInit} from '@angular/core';
 import {Receipt} from "../../model/receipt";
 import {OcrService} from "../../service/ocr.service";
 import {ReceiptService} from "../../service/receipt.service";
+import {AuthenticationService} from "../../service/authentication.service";
+import {User} from "../../model/user";
 
 @Component({
   selector: 'app-scanner',
@@ -15,8 +17,17 @@ export class ScannerComponent implements OnInit {
   fileToUpload: File;
   url: string;
   progress: boolean;
+  user: User = {
+    login: '',
+    name: '',
+    password: '',
+    email: ''
+  };
 
-  constructor(private ocrService: OcrService, private receiptService: ReceiptService) {
+  constructor(private ocrService: OcrService, private receiptService: ReceiptService, private userService: AuthenticationService) {
+    this.userService.getUser().subscribe(n => {
+       this.user = n;
+    });
     this.clearReceipt();
     this.fileToUpload = null;
     this.url = '';
@@ -63,6 +74,7 @@ export class ScannerComponent implements OnInit {
   }
 
   saveReceipt() {
+      this.receipt.user = this.user;
       this.receiptService.saveReceipt(this.receipt).subscribe(n => {
          this.clearReceipt();
       });
